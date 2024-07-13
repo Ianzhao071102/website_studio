6c342ad21b5411653951a1f6fc4627af964919c3
Ianzhao071102 <111406943+Ianzhao071102@users.noreply.github.com>
2024-07-13 09:12:11 +0800

    metrics support

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 import TabNavBar from "./components/TabNavBar.vue";
 import {RouterView} from "vue-router";
 import Banner from "./Banner.vue";
-
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
   <TabNavBar></TabNavBar>
   <Banner text="Check out our new Papers" header="Welcome"></Banner>
+  <SpeedInsights/>
   <router-view v-slot="{ Component, route }">
     <transition name="fade">
       <component :is="Component" :key="route.path" />
